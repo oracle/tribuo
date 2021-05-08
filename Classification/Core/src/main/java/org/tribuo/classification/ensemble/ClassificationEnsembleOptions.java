41e6f8645998984443868da6a869fa8dc773a462
Adam Pocock <adam.pocock@oracle.com>
2021-05-07 22:19:20 -0400

    Adding extra trees to ClassificationEnsembleOptions.

--- Classification/Core/src/main/java/org/tribuo/classification/ensemble/ClassificationEnsembleOptions.java
+++ Classification/Core/src/main/java/org/tribuo/classification/ensemble/ClassificationEnsembleOptions.java
@@ -21,6 +21,7 @@ import com.oracle.labs.mlrg.olcut.config.Options;
 import org.tribuo.Trainer;
 import org.tribuo.classification.Label;
 import org.tribuo.common.tree.DecisionTreeTrainer;
+import org.tribuo.common.tree.ExtraTreesTrainer;
 import org.tribuo.common.tree.RandomForestTrainer;
 import org.tribuo.ensemble.BaggingTrainer;
 
@@ -31,7 +32,28 @@ import java.util.logging.Logger;
  */
 public class ClassificationEnsembleOptions implements Options {
     private static final Logger logger = Logger.getLogger(ClassificationEnsembleOptions.class.getName());
-    public enum EnsembleType { ADABOOST, BAGGING, RF }
+
+    /**
+     * The type of ensemble.
+     */
+    public enum EnsembleType {
+        /**
+         * Creates an {@link AdaBoostTrainer}.
+         */
+        ADABOOST,
+        /**
+         * Creates a {@link BaggingTrainer}.
+         */
+        BAGGING,
+        /**
+         * Creates an {@link ExtraTreesTrainer}.
+         */
+        EXTRA_TREES,
+        /**
+         * Creates a {@link RandomForestTrainer}.
+         */
+        RF
+    }
 
     @Option(longName="ensemble-type",usage="Ensemble method, options are {ADABOOST, BAGGING, RF}.")
     public EnsembleType type = EnsembleType.BAGGING;
@@ -40,7 +62,11 @@ public class ClassificationEnsembleOptions implements Options {
     @Option(longName="ensemble-seed",usage="RNG seed.")
     public long seed = Trainer.DEFAULT_SEED;
 
-
+    /**
+     * Wraps the supplied trainer using the ensemble trainer described by these options.
+     * @param trainer The trainer to wrap.
+     * @return An ensemble trainer.
+     */
     public Trainer<Label> wrapTrainer(Trainer<Label> trainer) {
         if ((ensembleSize > 0) && (type != null)) {
             switch (type) {
@@ -50,6 +76,13 @@ public class ClassificationEnsembleOptions implements Options {
                 case BAGGING:
                     logger.info("Using Bagging with " + ensembleSize + " members.");
                     return new BaggingTrainer<>(trainer,new VotingCombiner(),ensembleSize,seed);
+                case EXTRA_TREES:
+                    if (trainer instanceof DecisionTreeTrainer) {
+                        logger.info("Using Extra Trees with " + ensembleSize + " members.");
+                        return new ExtraTreesTrainer<>((DecisionTreeTrainer<Label>)trainer,new VotingCombiner(),ensembleSize,seed);
+                    } else {
+                        throw new IllegalArgumentException("ExtraTreesTrainer requires a DecisionTreeTrainer");
+                    }
                 case RF:
                     if (trainer instanceof DecisionTreeTrainer) {
                         logger.info("Using Random Forests with " + ensembleSize + " members.");
