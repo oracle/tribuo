7647d10f6e93751d8ed24a0142d6fa6f95ca5a3c
Adam Pocock <adam.pocock@oracle.com>
2021-07-14 23:10:29 -0400

    Fixing formatting in FieldResponseProcessor.

--- Data/src/main/java/org/tribuo/data/columnar/processors/response/FieldResponseProcessor.java
+++ Data/src/main/java/org/tribuo/data/columnar/processors/response/FieldResponseProcessor.java
@@ -199,8 +199,7 @@ public class FieldResponseProcessor<T extends Output<T>> implements ResponseProc
             String val;
             if (uppercase) {
                 val = values.get(i).toUpperCase().trim();
-            }
-            else {
+            } else {
                 val = values.get(i).trim();
             }
             val = val.isEmpty() ? defaultValues.get(i) : val;
