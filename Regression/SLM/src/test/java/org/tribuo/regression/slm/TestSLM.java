53d9fd1fee3b70d3cb3f47ff8175b2ea715ddd3c
Adam Pocock <adam.pocock@oracle.com>
2021-08-20 21:47:49 -0400

    Re-enable the arm64 check.

--- Regression/SLM/src/test/java/org/tribuo/regression/slm/TestSLM.java
+++ Regression/SLM/src/test/java/org/tribuo/regression/slm/TestSLM.java
@@ -103,7 +103,7 @@ public class TestSLM {
                 }
 
                 String arch = System.getProperty("os.arch");
-                //if (arch.equalsIgnoreCase("amd64") || arch.equalsIgnoreCase("x86_64")) {
+                if (arch.equalsIgnoreCase("amd64") || arch.equalsIgnoreCase("x86_64")) {
                     // Initialise the OrtEnvironment to load the native library
                     // (as OrtSession.SessionOptions doesn't trigger the static initializer).
                     OrtEnvironment env = OrtEnvironment.getEnvironment();
@@ -123,9 +123,9 @@ public class TestSLM {
                         assertArrayEquals(tribuo.getOutput().getValues(), external.getOutput().getValues(), 1e-5);
                     }
                     onnxModel.close();
-                //} else {
-                //    logger.warning("ORT based tests only supported on x86_64, found " + arch);
-                //}
+                } else {
+                    logger.warning("ORT based tests only supported on x86_64, found " + arch);
+                }
 
                 onnxFile.toFile().delete();
             } catch (IOException | OrtException ex) {
