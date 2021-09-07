8ec8d6bc7834bca400a2d59f2d8b95d499af9ec7
Adam Pocock <adam.pocock@oracle.com>
2021-09-07 16:16:31 -0400

    Adding downcast as a parameter to ONNXUtil.arrayBuilder.

--- Core/src/main/java/org/tribuo/onnx/ONNXUtils.java
+++ Core/src/main/java/org/tribuo/onnx/ONNXUtils.java
@@ -21,6 +21,7 @@ import com.google.protobuf.ByteString;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.DoubleBuffer;
 import java.nio.FloatBuffer;
 
 /**
@@ -60,16 +61,38 @@ public abstract class ONNXUtils {
      * @return A TensorProto containing the array as floats.
      */
     public static OnnxMl.TensorProto arrayBuilder(ONNXContext context, String name, double[] parameters) {
+        return arrayBuilder(context,name,parameters,true);
+    }
+
+    /**
+     * Builds a TensorProto containing the array.
+     * <p>
+     * Optionally downcasts the doubles into floats.
+     * @param context The naming context.
+     * @param name The base name for the proto.
+     * @param parameters The array to store in the proto.
+     * @param downcast Downcasts the doubles into floats.
+     * @return A TensorProto containing the array as either floats or doubles.
+     */
+    public static OnnxMl.TensorProto arrayBuilder(ONNXContext context, String name, double[] parameters, boolean downcast) {
         OnnxMl.TensorProto.Builder arrBuilder = OnnxMl.TensorProto.newBuilder();
         arrBuilder.setName(context.generateUniqueName(name));
         arrBuilder.addDims(parameters.length);
-        arrBuilder.setDataType(OnnxMl.TensorProto.DataType.FLOAT.getNumber());
-        ByteBuffer buffer = ByteBuffer.allocate(parameters.length*4).order(ByteOrder.LITTLE_ENDIAN);
-        FloatBuffer floatBuffer = buffer.asFloatBuffer();
-        for (int i = 0; i < parameters.length; i++) {
-            floatBuffer.put((float)parameters[i]);
+        int capacity = downcast ? parameters.length * 4 : parameters.length * 8;
+        ByteBuffer buffer = ByteBuffer.allocate(capacity).order(ByteOrder.LITTLE_ENDIAN);
+        if (downcast) {
+            arrBuilder.setDataType(OnnxMl.TensorProto.DataType.FLOAT.getNumber());
+            FloatBuffer floatBuffer = buffer.asFloatBuffer();
+            for (int i = 0; i < parameters.length; i++) {
+                floatBuffer.put((float) parameters[i]);
+            }
+            floatBuffer.rewind();
+        } else {
+            arrBuilder.setDataType(OnnxMl.TensorProto.DataType.DOUBLE.getNumber());
+            DoubleBuffer doubleBuffer = buffer.asDoubleBuffer();
+            doubleBuffer.put(parameters);
+            doubleBuffer.rewind();
         }
-        floatBuffer.rewind();
         arrBuilder.setRawData(ByteString.copyFrom(buffer));
         return arrBuilder.build();
     }
