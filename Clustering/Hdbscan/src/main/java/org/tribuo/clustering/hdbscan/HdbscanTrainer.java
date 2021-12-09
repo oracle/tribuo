ec4499ff31228900ade8bdef4e60db7cc1c95c1b
Geoffrey Stewart <geoff.stewart@oracle.com>
2021-12-09 14:42:01 -0800

    Remove the unused import
    Signed-off-by: Geoffrey Stewart <geoff.stewart@oracle.com>

--- Clustering/Hdbscan/src/main/java/org/tribuo/clustering/hdbscan/HdbscanTrainer.java
+++ Clustering/Hdbscan/src/main/java/org/tribuo/clustering/hdbscan/HdbscanTrainer.java
@@ -39,7 +39,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
