b409ba0d23a4834d35c72d926b46b1854487805b
Adam Pocock <adam.pocock@oracle.com>
2022-03-15 14:34:05 -0400

    Adds support for merging MeanVarianceAccumulators (#215)
    * Adding a merge operation to MeanVarianceAccumulator.
    
    * Fixing missing copyright statements.
    
    * Fixing the merging of MeanVarianceAccumulators as the mean merge was assuming they were the same size. Updated the test to check for this.

--- Core/src/test/java/org/tribuo/util/MeanVarianceAccumulatorTest.java
+++ Core/src/test/java/org/tribuo/util/MeanVarianceAccumulatorTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2020, 2022, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.tribuo.util;
 
 import com.oracle.labs.mlrg.olcut.util.Pair;
@@ -72,6 +87,36 @@ public class MeanVarianceAccumulatorTest {
         Pair<Double, Double> meanVarPair = Util.meanAndVariance(values);
         Assertions.assertEquals(meanVarPair.getA(),accumulator.getMean(), DELTA);
         Assertions.assertEquals(meanVarPair.getB(),accumulator.getVariance(), DELTA);
+    }
+
+    @Test
+    public void testMerge() {
+        SplittableRandom rng = new SplittableRandom(Trainer.DEFAULT_SEED);
+
+        double[] values = new double[4096];
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = rng.nextDouble();
+        }
+
+        double[] firstValues = new double[3072];
+        double[] secondValues = new double[1024];
+
+        System.arraycopy(values, 0, firstValues, 0, firstValues.length);
+        System.arraycopy(values, 3072, secondValues, 0, secondValues.length);
+
+        MeanVarianceAccumulator totalAccumulator = new MeanVarianceAccumulator(values);
+
+        MeanVarianceAccumulator first = new MeanVarianceAccumulator(firstValues);
+        MeanVarianceAccumulator second = new MeanVarianceAccumulator(secondValues);
+
+        MeanVarianceAccumulator sum = MeanVarianceAccumulator.merge(first,second);
+
+        Assertions.assertEquals(totalAccumulator.getMin(),sum.getMin(), DELTA);
+        Assertions.assertEquals(totalAccumulator.getMax(),sum.getMax(), DELTA);
+        Assertions.assertEquals(totalAccumulator.getMean(),sum.getMean(), DELTA);
+        Assertions.assertEquals(totalAccumulator.getVariance(),sum.getVariance(), DELTA);
+        Assertions.assertEquals(totalAccumulator.getCount(),sum.getCount());
     }
 
 }
