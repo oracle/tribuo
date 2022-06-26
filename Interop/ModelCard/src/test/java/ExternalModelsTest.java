5fc80136c3a812e964a4a1be7459133428c25410
Romina Mahinpei <mahinpei@student.ubc.ca>
2022-06-25 18:18:55 -0700

    Use self-generated testing data files
    Signed-off-by: Romina Mahinpei <mahinpei@student.ubc.ca>

--- Interop/ModelCard/src/test/java/ExternalModelsTest.java
+++ Interop/ModelCard/src/test/java/ExternalModelsTest.java
@@ -21,7 +21,7 @@ import org.tribuo.classification.Label;
 import org.tribuo.classification.LabelFactory;
 import org.tribuo.classification.xgboost.XGBoostClassificationConverter;
 import org.tribuo.common.xgboost.XGBoostExternalModel;
-import org.tribuo.datasource.IDXDataSource;
+import org.tribuo.data.csv.CSVLoader;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -34,27 +34,28 @@ public class ExternalModelsTest {
     @Test
     public void testExternalModelException() throws IOException {
         var labelFactory = new LabelFactory();
-        var mnistTrainSource = new IDXDataSource<>(Paths.get("src/test/input-data/train-images-idx3-ubyte.gz"),Paths.get("src/test/input-data/train-labels-idx1-ubyte.gz"),labelFactory);
-        var mnistTestSource = new IDXDataSource<>(Paths.get("src/test/input-data/t10k-images-idx3-ubyte.gz"),Paths.get("src/test/input-data/t10k-labels-idx1-ubyte.gz"),labelFactory);
-        var mnistTrain = new MutableDataset<>(mnistTrainSource);
-        var mnistTest = new MutableDataset<>(mnistTestSource);
+        var csvLoader = new CSVLoader<>(labelFactory);
+
+        var dataSource = csvLoader.loadDataSource(Paths.get("src/test/resources/externalModelSampleData.csv"), "response");
+        var trainData = new MutableDataset<>(dataSource);
+        var evalData = new MutableDataset<>(dataSource);
 
         var xgbLabelConv = new XGBoostClassificationConverter();
-        var xgbModelPath = Paths.get("src/test/input-data/xgb_mnist.xgb");
+        var xgbModelPath = Paths.get("src/test/resources/externalModelPath.xgb");
 
         Map<String, Integer> xgbFeatMapping = new HashMap<>();
         for (int i = 0; i < 784; i++) {
-            // This MNIST model has the feature indices transposed to test a non-trivial mapping.
             int id = (783 - i);
             xgbFeatMapping.put(String.format("%03d", i), id);
         }
         Map<Label, Integer> xgbOutMapping = new HashMap<>();
-        for (Label l : mnistTrain.getOutputInfo().getDomain()) {
-            xgbOutMapping.put(l, Integer.parseInt(l.getLabel()));
+        int i = 0;
+        for (Label l : trainData.getOutputInfo().getDomain()) {
+            xgbOutMapping.put(l, i);
+            i++;
         }
         Model<Label> xgbModel = XGBoostExternalModel.createXGBoostModel(labelFactory, xgbFeatMapping, xgbOutMapping, xgbLabelConv, xgbModelPath);
-        var xgbEvaluation = labelFactory.getEvaluator().evaluate(xgbModel,mnistTest);
-
+        var xgbEvaluation = labelFactory.getEvaluator().evaluate(xgbModel,evalData);
         try {
             ModelCard modelCard = new ModelCard(xgbModel, xgbEvaluation);
             fail("Exception expected");
