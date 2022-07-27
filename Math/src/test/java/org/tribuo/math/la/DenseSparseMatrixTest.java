c93cdd35f405b5ce0503674a9dc666783fb7fa1c
Philip Ogren <philip.ogren@oracle.com>
2022-07-27 17:52:27 -0600

    added simple test for DenseSparseMatrix.getColumn

--- Math/src/test/java/org/tribuo/math/la/DenseSparseMatrixTest.java
+++ Math/src/test/java/org/tribuo/math/la/DenseSparseMatrixTest.java
@@ -22,7 +22,17 @@ public class DenseSparseMatrixTest {
         diagonal = DenseSparseMatrix.createDiagonal(new DenseVector(new double[] {1.618033988749894, Math.E, Math.PI}));
         assertMatrixEquals(new DenseMatrix(new double[][]{new double[]{1.618033988749894, 0.0, 0.0}, new double[]{0.0, 2.718281828459045, 0.0}, new double[]{0.0, 0.0, 3.141592653589793}}),diagonal);
     }
-    
+
+    @Test
+    public void testGetColumn() {
+        DenseSparseMatrix diagonal = DenseSparseMatrix.createDiagonal(new DenseVector(new double[] {1.618033988749894, Math.E, Math.PI}));
+        SparseVector column = diagonal.getColumn(1);
+        assertEquals(3, column.size());
+        assertEquals(0, column.get(0));
+        assertEquals(Math.E, column.get(1));
+        assertEquals(0, column.get(2));
+    }
+
     public static void assertMatrixEquals(Matrix expected, Matrix actual) {
         assertEquals(expected.getDimension1Size(), actual.getDimension1Size(), "dim1 differs");
         assertEquals(expected.getDimension2Size(), actual.getDimension2Size(), "dim2 differs");
