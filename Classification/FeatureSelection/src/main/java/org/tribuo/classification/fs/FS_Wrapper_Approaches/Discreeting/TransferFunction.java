4f7a25f0f20877baa8bdf0bdd0904ed8311ec66c
Mohammed Al-Eiadeh <93108547+Mohammed-Ryiad-Eiadeh@users.noreply.github.com>
2023-04-27 04:40:56 +0300

    Update TransferFunction.java

--- Classification/FeatureSelection/src/main/java/org/tribuo/classification/fs/FS_Wrapper_Approaches/Discreeting/TransferFunction.java
+++ Classification/FeatureSelection/src/main/java/org/tribuo/classification/fs/FS_Wrapper_Approaches/Discreeting/TransferFunction.java
@@ -4,5 +4,5 @@ package FS_Wrapper_Approaches.Discreeting;
  * Enumeration that contains the types of transfer functions in which they are used to define the type of transfer function
  */
 public enum TransferFunction {
-        V1, V2, V3, V4
+        V1, V2, V3, V4, S1, S2, S3, S4
 }
