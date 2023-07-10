8c9d3e2aacf1ec1e2abfc7a8be1aca124b9616a2
Mohammed Al-Eiadeh <93108547+Mohammed-Ryiad-Eiadeh@users.noreply.github.com>
2023-07-10 13:27:01 +0300

    Update TransferFunction.java

--- Classification/FeatureSelection/src/main/java/org/tribuo/classification/fs/FS_Wrapper_Approaches/Discreeting/TransferFunction.java
+++ Classification/FeatureSelection/src/main/java/org/tribuo/classification/fs/FS_Wrapper_Approaches/Discreeting/TransferFunction.java
@@ -1,8 +1,32 @@
 package FS_Wrapper_Approaches.Discreeting;
 
+import org.ojalgo.function.special.ErrorFunction;
+
+import java.util.function.DoubleUnaryOperator;
+
 /**
  * Enumeration that contains the types of transfer functions in which they are used to define the type of transfer function
  */
-public enum TransferFunction {
-        V1, V2, V3, V4, S1, S2, S3, S4
+public enum TransferFunction implements DoubleUnaryOperator {
+        V1, V2, V3, V4, S1, S2, S3, S4;
+
+        /**
+         * Applies this operator to the given value.
+         *
+         * @param value the operand as continuous value to be converted to either 1 or 0
+         * @return the operator result that is a d
+         */
+        @Override
+        public double applyAsDouble(double value) {
+                return switch (this) {
+                        case V1 -> Math.abs(ErrorFunction.erf(Math.sqrt(Math.PI) / 2 * value)) >= 0.5 ? 1 : 0;
+                        case V2 -> Math.abs(Math.tan(value)) >= 0.5 ? 1 : 0;
+                        case V3 -> Math.abs(value / Math.abs(1 + Math.pow(value, 2))) >= 0.5 ? 1 : 0;
+                        case V4 -> Math.abs(2 / Math.PI * Math.atan(Math.PI / 2 * value)) >= 0.5 ? 1 : 0;
+                        case S1 -> 1 / (1 + Math.pow(Math.E, - 2 * value)) >= 0.5 ? 1 : 0;
+                        case S2 -> 1 / (1 + Math.pow(Math.E, - value)) >= 0.5 ? 1 : 0;
+                        case S3 -> 1 / (1 + Math.pow(Math.E, - value / 2)) >= 0.5 ? 1 : 0;
+                        case S4 -> 1 / (1 + Math.pow(Math.E, - value / 3)) >= 0.5 ? 1 : 0;
+                };
+        }
 }
