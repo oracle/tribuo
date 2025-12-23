0a8ec27e2f04cbfda983ab037b484c28d85f90db
Adam Pocock <adam.pocock@oracle.com>
2025-12-23 17:43:57 -0500

    Adding deserialization cache.

---
+++ Core/src/main/java/org/tribuo/protos/ProtoDeserializationCache.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tribuo.protos;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.tribuo.FeatureMap;
+import org.tribuo.OutputInfo;
+
+/**
+ * A cache used to dedupe objects during deserialization.
+ * <p>
+ * Supports deduping {@link FeatureMap} and {@link OutputInfo} instances as these are the largest
+ * shared structures. Feature map instances which are equal according to {@link FeatureMap#equals}
+ * will be deduplicated by returning the canonical instance.
+ */
+public final class ProtoDeserializationCache {
+
+    private final Map<FeatureMap, FeatureMap> featureMapCache;
+
+    private final Map<OutputInfo<?>, OutputInfo<?>> outputInfoCache;
+
+    /**
+     * Create an empty deserialization cache.
+     */
+    public ProtoDeserializationCache() {
+        this.featureMapCache = new HashMap<>();
+        this.outputInfoCache = new HashMap<>();
+    }
+
+    /**
+     * Canonicalises the feature map by checking if we've already seen this one, caching the value
+     * if not, and returning an equal feature map.
+     * @param featureMap The feature map to canonicalise.
+     * @return The canonicalised value (i.e., a cached one which is equal to the method argument).
+     */
+    public FeatureMap canconicalise(FeatureMap featureMap) {
+        FeatureMap canonicalisedValue = featureMapCache.computeIfAbsent(featureMap, k -> featureMap);
+        return canonicalisedValue;
+    }
+
+    /**
+     * Canonicalises the output info by checking if we've already seen this one, caching the value
+     * if not, and returning an equal output info.
+     * @param outputInfo The output info to canonicalise.
+     * @return The canonicalised value (i.e., a cached one which is equal to the method argument).
+     */
+    public OutputInfo<?> canconicalise(OutputInfo<?> outputInfo) {
+        OutputInfo<?> canonicalisedValue = outputInfoCache.computeIfAbsent(outputInfo, k -> outputInfo);
+        return canonicalisedValue;
+    }
+
+}
+
