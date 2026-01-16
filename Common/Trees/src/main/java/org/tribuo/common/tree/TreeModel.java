fe50047941e3975c81ee26e1fb4cc926c52ed7f8
Adam Pocock <adam.pocock@oracle.com>
2026-01-15 22:03:03 -0500

    Changing Node.getNextNode(SparseVector) to accept SGDVector.

--- Common/Trees/src/main/java/org/tribuo/common/tree/TreeModel.java
+++ Common/Trees/src/main/java/org/tribuo/common/tree/TreeModel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2026, Oracle and/or its affiliates. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,6 @@ import org.tribuo.common.tree.protos.TreeModelProto;
 import org.tribuo.common.tree.protos.TreeNodeProto;
 import org.tribuo.impl.ModelDataCarrier;
 import org.tribuo.math.la.SGDVector;
-import org.tribuo.math.la.SparseVector;
 import org.tribuo.protos.core.ModelProto;
 import org.tribuo.provenance.ModelProvenance;
 
@@ -313,7 +312,7 @@ public class TreeModel<T extends Output<T>> extends SparseModel<T> {
     public Prediction<T> predict(Example<T> example) {
         //
         // Ensures we handle collisions correctly
-        SparseVector vec = SparseVector.createSparseVector(example,featureIDMap,false);
+        SGDVector vec = SGDVector.createFromExample(example, featureIDMap, false);
         return predict(vec, example);
     }
 
@@ -361,7 +360,7 @@ public class TreeModel<T extends Output<T>> extends SparseModel<T> {
 
         while (curNode != null) {
             oldNode = curNode;
-            curNode = oldNode.getNextNode((SparseVector) vec);
+            curNode = oldNode.getNextNode(vec);
         }
 
         //
@@ -402,7 +401,7 @@ public class TreeModel<T extends Output<T>> extends SparseModel<T> {
             }
         }
         List<Pair<String,Double>> list = new ArrayList<>();
-        while (q.size() > 0) {
+        while (!q.isEmpty()) {
             list.add(q.poll());
         }
         Collections.reverse(list);
@@ -418,7 +417,7 @@ public class TreeModel<T extends Output<T>> extends SparseModel<T> {
         List<String> list = new ArrayList<>();
         //
         // Ensures we handle collisions correctly
-        SparseVector vec = SparseVector.createSparseVector(example,featureIDMap,false);
+        SGDVector vec = SGDVector.createFromExample(example, featureIDMap, false);
         Node<T> oldNode = root;
         Node<T> curNode = root;
 
